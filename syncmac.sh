b1d092cff93b2258a05f6f462d9c499b3e45aa49
unidevel <unidevel@hotmail.com>
2016-10-08 10:23:14 +0800

    sync mac

---
+++ syncmac.sh
@@ -0,0 +1,3 @@
+cp createstep.js /Applications/wechatwebdevtools.app/Contents/Resources/app.nw/app/dist/components/create/
+cp projectStores.js /Applications/wechatwebdevtools.app/Contents/Resources/app.nw/app/dist/stores/
+cp asdebug.js /Applications/wechatwebdevtools.app/Contents/Resources/app.nw/app/dist/weapp/appservice/
